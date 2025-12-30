79657a21b87e93a3623144c4b05e35235239e4dd
DAS_Desktop01 <edvardsystem@gmail.com>
2025-12-30 12:23:37 +0200

    Inceputul programului

--- EditarePDF/MainWindow.xaml.cs
+++ EditarePDF/MainWindow.xaml.cs
@@ -20,5 +20,31 @@ namespace EditarePDF
         {
             InitializeComponent();
         }
+
+        private void OpenPdf_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Add logic to open a PDF file
+        }
+
+        private void Eraser_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement eraser functionality here
+        }
+
+        private void ConvertToWord_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement PDF to Word conversion logic here
+            MessageBox.Show("Convertire Word funcționalitate nu este încă implementată.");
+        }
+
+        private void EditorCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // TODO: Add your mouse down logic here
+        }
+
+        private void EditorCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            // TODO: Add your mouse move logic here
+        }
     }
 }
